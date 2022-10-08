173e7d1f91dc873e2d75055e4dfacb093c783d34
yolly98 <gianlucagemini98@gmail.com>
2022-10-08 11:16:36 +0000

    updated arch folder

--- mqtt-node/project-conf.h
+++ mqtt-node/project-conf.h
@@ -4,4 +4,11 @@
 /* Enable TCP */
 #define UIP_CONF_TCP 1
 
+/* for launchpad */
+//#define CCXXWARE_CONF_ROM_BOOTLOADER_ENABLE 1
+//#define IEEE802154_CONF_PANID   0xABCD
+//#define IEEE802154_CONF_DEFAULT_CHANNEL 25
+//#define RF_BLE_CONF_ENABLED 1
+/* --- */
+
 #endif /* PROJECT_CONF_H_ */
